61ac00e14651f05f8e98f0a06363a8859626ffc7
Pienapples <frizadiza@gmail.com>
2023-12-01 06:31:02 -0600

    Update cfgFunctions.hpp

--- Functions/cfgFunctions.hpp
+++ Functions/cfgFunctions.hpp
@@ -1,9 +1,10 @@
+// To be able to use Functions in any scripts throughout the mission file, you need to define the cfgFunctions in the description.ext file inside the mission folder.
 class ER
 {
     tag ="ER";
     class Clients
     {
-        file = "Functions\Clients";
-        class AttachAndSetVector {}; 
+        file = "Functions\Clients"; // File path to the function
+        class AttachAndSetVector {}; // Follows the same name as the .sqf without the "fn_" at the start.
     };
 };
