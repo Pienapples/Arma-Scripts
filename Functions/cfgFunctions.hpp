1335dda66eab01a05646f00b1338f2b4a313612e
Pienapples <frizadiza@gmail.com>
2023-12-01 07:25:16 -0600

    Update cfgFunctions.hpp

--- Functions/cfgFunctions.hpp
+++ Functions/cfgFunctions.hpp
@@ -1,7 +1,7 @@
 // To be able to use Functions in any scripts throughout the mission file, you need to define the cfgFunctions in the description.ext file inside the mission folder.
 class ER
 {
-    tag ="ER";
+    tag ="ER"; // The prefix used when calling the function; ER_fnc_myFunction
     class Clients
     {
         file = "Functions\Clients"; // File path to the function
